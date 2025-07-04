62e8889d81ae6a6c657792abb1b7eae91d2d0000
Adrian1998-java <scraggy1998@gmail.com>
2025-07-04 14:04:13 +0100

    Cambios

---
+++ blade/resources/views/components/card2.blade.php
@@ -0,0 +1,5 @@
+<div class="card1">
+    <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/teide.png') }}" alt="El Teide">
+    <p>{{ $content }}</p>
+</div>
--- blade/resources/views/services.blade.php
+++ blade/resources/views/services.blade.php
@@ -3,6 +3,58 @@
 @section('title', 'Services')
 
 @section('content')
+<main>
     <h1>Services</h1>
+    <div class="cards">
+        @component('components.card')
+            @slot('title', 'Service1')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service2')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'Service3')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service4')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'Service1')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service2')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'Service3')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service4')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'Service1')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service2')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'Service3')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+        @component('components.card2')
+            @slot('title', 'Service4')
+            @slot('content', 'Lorem ipsum dolor sit amet.')
+        @endcomponent
+    </div>
+</main>
 
 @endsection()
