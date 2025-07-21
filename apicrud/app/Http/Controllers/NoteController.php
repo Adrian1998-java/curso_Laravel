fd9289eabae0f978369604ab396212b81a043b7d
Adrian1998-java <scraggy1998@gmail.com>
2025-07-21 13:48:33 +0100

    API Crud Resolver problemas

---
+++ apicrud/app/Http/Controllers/NoteController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use App\Http\Requests\NoteResquest;
+use Illuminate\Http\JsonResponse;
+
+class NoteController extends Controller
+{
+
+    public function index() : JsonResponse
+    {
+        $notes = Note::all();
+        return response()->json($notes, 200);
+    }
+
+
+    public function store(NoteRequest $request) : JsonResponse
+    {
+        $note = Note::create($request->all());
+        return response()->json([
+            'success' => true,
+            'data' => $note
+        ], 201);
+        // $note = new Note();
+        // $note->title = $request->title;
+        // $note->content = $request->content;
+        // $note->save();
+
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $note
+        // ], 201);
+    }
+
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+
+    public function show(string $id) : JsonResponse
+    {
+        $note = Note::find($id);
+        return response()->json($note, 200);
+    }
+
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
+
+
+    public function update(NoteRequest $request, $id) : JsonResponse
+    {
+        $note = Note::find($id);
+        $note->title = $request->title;
+        $note->content = $request->content;
+        $note->save();
+
+        return response()->json([
+            'success' => true,
+            'data' => $note
+        ], 200);
+
+    }
+
+    public function destroy(string $id) : JsonResponse
+    {
+        Note::find($id)->delete();
+        return response()->json([
+            'success' => true
+        ], 200);
+    }
+}
---
+++ apicrud/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiting\Limit;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public const HOME = "/home";
+
+    public function boot()
+    {
+
+        $this->configureRateLimiting();
+    
+        $this->routes(function(){
+            Route::prefix('api')
+            ->group(base_path('routes/api.php'));
+    
+            Route::group(base_path('routes/web.php'));
+        });
+    }
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user()->id ?: $request-> ip()));
+
+        });
+    }
+}
+
+
